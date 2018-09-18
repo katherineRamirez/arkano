6cd0a72cb321388b2a4fcb776d5ceef2b24b5ca5
Katherine Ramirez <oeillet8_5@hotmail.com>
2018-09-17 23:44:59 -0300

    función validar

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,46 +8,80 @@ function w3_close() {
 
 // Validación Página de alta datos 
 function validacion() {
-	var nombre = document.getElementById("nombreDashboard").value;
-	var nuevasCompras = document.getElementById("nuevasCompras").value;
-	var incrementoCompras = document.getElementById("incrementoCompras").value;
-	var nuevosUsuarios = document.getElementById("nuevosUsuarios").value;
-	var nuevasVisitas = document.getElementById("nuevasVisitas").value;
-	var fecha = document.getElementById("fecha").value; 
+	// Validando nombre de dashboard
+	var tarNombreDashboard = document.getElementById("tarNombreDashboard");
+	var nombreDashboard = document.getElementById("nombreDashboard").value;
 
+	if (nombreDashboard == "" || nombreDashboard == null || nombreDashboard.length == 0 || !isNaN(nombreDashboard) || /^\s+$/.test(nombreDashboard)) {
+    alert('Escriba un nombre correcto');
+   }else{
+
+	var pNombreDashboard = document.createElement('h2');
+	var tNombreDashboard =  document.createTextNode(nombreDashboard);
+	pNombreDashboard.appendChild(tNombreDashboard);
+	tarNombreDashboard.appendChild(pNombreDashboard);
+}
+	// Validando nuevas compras
 	var tarNuevasCompras = document.getElementById("tarNuevasCompras");
-
-	if( nombre == null || nombre.length == 0) {
-		// Si no se cumple la condicion...
-		alert('Introduzca un nombre válido');
-	}else {
-		console.log("ok")
-	}
-
+	var nuevasCompras = document.getElementById("nuevasCompras").value;
 	if (isNaN(nuevasCompras)) {
     alert('Introduzca sólo números');
-    return false;
-  }else
+   }else{
 
-  if (isNaN(incrementoCompras)) {
+	var pNuevasCompras = document.createElement('h2');
+	var tNuevasCompras =  document.createTextNode(nuevasCompras);
+	pNuevasCompras.appendChild(tNuevasCompras);
+	tarNuevasCompras.appendChild(pNuevasCompras);
+}
+
+	// Validando incremento de compras
+	var tarIncrementoCompras = document.getElementById("tarIncrementoCompras");
+	var incrementoCompras = document.getElementById("incrementoCompras").value;
+	  if (isNaN(incrementoCompras)) {
     alert('Introduzca sólo números');
-    return false;
-  }else
+   }else{
 
+	var pIncrementoCompras = document.createElement('h2');
+	var tIncrementoCompras =  document.createTextNode(incrementoCompras);
+	pIncrementoCompras.appendChild(tIncrementoCompras);
+	tarIncrementoCompras.appendChild(pIncrementoCompras);
+}
+	// Validando nuevos usuarios
+	var tarNuevosUsuarios = document.getElementById("tarNuevosUsuarios");
+	var nuevosUsuarios = document.getElementById("nuevosUsuarios").value;
   if (isNaN(nuevosUsuarios)) {
     alert('Introduzca sólo números');
-    return false;
-  }else
+   }else{
 
-  if (isNaN(nuevasVisitas)) {
+	var pNuevosUsuarios = document.createElement('h2');
+	var tNuevosUsuarios =  document.createTextNode(nuevosUsuarios);
+	pNuevosUsuarios.appendChild(tNuevosUsuarios);
+	tarNuevosUsuarios.appendChild(pNuevosUsuarios);
+}
+	// Validando nuevas visitas
+	var tarNuevasVisitas = document.getElementById("tarNuevasVisitas");
+	var nuevasVisitas = document.getElementById("nuevasVisitas").value;
+	if (isNaN(nuevasVisitas)) {
     alert('Introduzca sólo números');
-    return false;
-  }else{
+   }else{
 
-  	return true;
-  }
+	var pNuevasVisitas = document.createElement('h2');
+	var tNuevasVisitas =  document.createTextNode(nuevasVisitas);
+	pNuevasVisitas.appendChild(tNuevasVisitas);
+	tarNuevasVisitas.appendChild(pNuevasVisitas);
+}
+	// Validando la fecha
+	var tarFecha = document.getElementById("tarFecha");
+	var fecha = document.getElementById("fecha").value;
+
+	var pFecha = document.createElement('h2');
+	var tFecha =  document.createTextNode(fecha);
+	pFecha.appendChild(tFecha);
+	tarFecha.appendChild(pFecha);
+
 
 }
+
 
 // Página de dashboard 
 	var pAltaDatos = document.getElementById("pAltaDatos");
@@ -70,7 +104,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=santiago&units=metric&AP
     return response.json();
   })
   .then(function(data) {
-    console.log(data);
+    //console.log(data);
     var dataWeather = data.weather;
     for(var i = 0; i < dataWeather.length; i++){
     	var pName = document.createElement("h2");
